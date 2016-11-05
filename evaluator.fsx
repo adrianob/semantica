83646eb60637c90d3d241ebf3f91483b6846f413
Adriano Benin <adriano.benin@gmail.com>
2016-11-05 20:04:11 -0200

    add a few more rules

--- evaluator.fsx
+++ evaluator.fsx
@@ -1,25 +1,35 @@
-(* Gramatica : t ::= true | false | if ( t1 , t2 , t3 ) | 0 | succ ( t ) | pred ( t ) | iszero ( t ) *)
 type term =
+  | TmN of int
   | TmTrue
   | TmFalse
+  | TmPlus of term * term
+  | TmMinus of term * term
+  | TmApp of term * term
+  | TmNil
+  | TmVar of string
   | TmIf of term * term * term
+  | TmFun of term * term
   | TmZero
   | TmSucc of term
-  | TmPred of term
+  | TmIsEmpty of term
+  | TmHd of term
+  | TmTl of term
+  | TmTry of term * term
   | TmIsZero of term
+
 (* Excecao a ser ativada quando termo for uma FORMA NORMAL *)
 exception NoRuleApplies
 (* Funcao auxiliar para determinar se um termo e um VALOR NUMERICO *)
 let rec isnumericval t =
   match t with
-  | TmZero -> true
+  TmZero -> true
   | TmSucc ( t1 ) -> isnumericval t1
   | _ -> false
 (* Implementacao da funcao STEP de avaliacao em um passo *)
 let rec step t =
   match t with
 (* CASO IF ( t1 , t2 , t3 ) *)
-  | TmIf ( TmTrue , t2 , t3 ) -> (* regra E−IfTrue *)
+  TmIf ( TmTrue , t2 , t3 ) -> (* regra E−IfTrue *)
       t2
   | TmIf ( TmFalse , t2 , t3 ) -> (* regra E−False *)
     t3
@@ -30,14 +40,6 @@ let rec step t =
   | TmSucc ( t1 ) -> (* regra E−Succ *)
     let t1' = step t1 in
     TmSucc ( t1')
-(* CASO PRED ( t1 ) *)
-  | TmPred ( TmZero ) -> (* regra E−PredZero *)
-    TmZero
-  | TmPred ( TmSucc ( nv1 )) when ( isnumericval nv1 ) -> (* regra E−PredSucc *)
-    nv1
-  | TmPred ( t1 ) -> (* regra E−Pred *)
-    let t1' = step t1 in
-    TmPred ( t1')
 (* CASO ISZERO ( t1 ) *)
   | TmIsZero ( TmZero ) -> (* regra E−IsZeroZero *)
     TmTrue
@@ -46,6 +48,40 @@ let rec step t =
   | TmIsZero ( t1 ) -> (* regra E−IsZero *)
     let t1' = step t1 in
     TmIsZero ( t1')
+(* CASO + ( t1, t2 ) *)
+  | TmPlus ( TmN t1, TmN t2 )  ->
+    TmN (t1 + t2)
+
+  | TmPlus ( TmN t1, t2 )  ->
+    let t2' = step t2 in
+    TmPlus (TmN t1, t2')
+  | TmPlus ( t1, t2 )  ->
+    let t1' = step t1 in
+    TmPlus (t1', t2)
+(* CASO - ( t1, t2 ) *)
+  | TmMinus ( TmN t1, TmN t2 )  ->
+    TmN (t1 - t2)
+  | TmMinus ( TmN t1, t2 )  ->
+    let t2' = step t2 in
+    TmMinus (TmN t1, t2')
+  | TmMinus ( t1, t2 )  ->
+    let t1' = step t1 in
+    TmMinus (t1', t2)
+(* CASO t1 t2 *)
+  | TmApp (TmFun (TmVar t1, t2), TmN t3) ->
+    t2 (*TODO substituir var*)
+  | TmApp (TmFun (TmVar t1, t2), TmTrue) ->
+    t2 (*TODO substituir var*)
+  | TmApp (TmFun (TmVar t1, t2), TmFalse) ->
+    t2 (*TODO substituir var*)
+  | TmApp (TmFun (TmVar t1, t2), TmFun (t3, t4)) ->
+    t2 (*TODO substituir var*)
+  | TmApp (TmFun (TmVar t1, t2), t3) ->
+    let t3' = step t3 in
+    TmApp (TmFun (TmVar t1, t2), t3')
+  | TmApp (t1, t2) ->
+    let t1' = step t1 in
+    TmApp (t1' , t2)
 (* CASO Nenhuma regra se aplique ao termo *)
   | _ ->
     raise NoRuleApplies
@@ -61,4 +97,14 @@ let t3 = TmSucc ( TmZero )
 let tif = TmIf ( t1 , t2 , t3 )
 let t4 = TmIsZero ( TmSucc ( TmZero ))
 let t5 = TmIsZero ( TmFalse )
+let t10 = eval (TmN 10)
+let t11 = eval (TmPlus (TmN 10, TmN 2) )
+let t12 = eval (TmPlus (TmPlus (TmN 10, TmN 4), TmN 2) )
+let t13 = eval (TmMinus (TmPlus (TmN 10, TmN 4), TmN 2) )
 printf "%b" (eval t1 = TmTrue)
+match t10 with
+  | TmN c1 -> printf "%i" c1
+  | _ -> printf "nan"
+match t13 with
+  | TmN c1 -> printf "%i" c1
+  | _ -> printf "nan"
