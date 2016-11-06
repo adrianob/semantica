00f1da83ee6d473ee8e8e760e08b59296442ac07
Adriano Benin <adriano.benin@gmail.com>
2016-11-06 11:43:56 -0200

    start to implement substitution

--- evaluator.fsx
+++ evaluator.fsx
@@ -19,6 +19,17 @@ type term =
 
 (* Excecao a ser ativada quando termo for uma FORMA NORMAL *)
 exception NoRuleApplies
+let rec replaceTerm expression ident newTerm =
+  match expression with
+  | TmN t1 -> expression
+  | TmTrue -> expression
+  | TmFalse -> expression
+  | TmPlus (t1, t2) -> TmPlus (replaceTerm t1 ident newTerm, replaceTerm t2 ident newTerm)
+  | TmMinus (t1, t2) -> TmMinus (replaceTerm t1 ident newTerm, replaceTerm t2 ident newTerm)
+  | TmIf ( t1 , t2 , t3 ) -> TmIf (replaceTerm t1 ident newTerm, replaceTerm t2 ident newTerm, replaceTerm t3 ident newTerm)
+  | TmApp (t1, t2) -> TmApp (replaceTerm t1 ident newTerm, replaceTerm t2 ident newTerm)
+  | TmVar ident -> newTerm
+  | _ -> expression
 (* Funcao auxiliar para determinar se um termo e um VALOR NUMERICO *)
 let rec isnumericval t =
   match t with
@@ -51,7 +62,6 @@ let rec step t =
 (* CASO + ( t1, t2 ) *)
   | TmPlus ( TmN t1, TmN t2 )  ->
     TmN (t1 + t2)
-
   | TmPlus ( TmN t1, t2 )  ->
     let t2' = step t2 in
     TmPlus (TmN t1, t2')
@@ -69,13 +79,13 @@ let rec step t =
     TmMinus (t1', t2)
 (* CASO t1 t2 *)
   | TmApp (TmFun (TmVar t1, t2), TmN t3) ->
-    t2 (*TODO substituir var*)
+    replaceTerm t2 t1 (TmN t3)
   | TmApp (TmFun (TmVar t1, t2), TmTrue) ->
-    t2 (*TODO substituir var*)
+    replaceTerm t2 t1 TmTrue
   | TmApp (TmFun (TmVar t1, t2), TmFalse) ->
-    t2 (*TODO substituir var*)
+    replaceTerm t2 t1 TmFalse
   | TmApp (TmFun (TmVar t1, t2), TmFun (t3, t4)) ->
-    t2 (*TODO substituir var*)
+    replaceTerm t2 t1 (TmFun (t3, t4))
   | TmApp (TmFun (TmVar t1, t2), t3) ->
     let t3' = step t3 in
     TmApp (TmFun (TmVar t1, t2), t3')
@@ -101,10 +111,14 @@ let t10 = eval (TmN 10)
 let t11 = eval (TmPlus (TmN 10, TmN 2) )
 let t12 = eval (TmPlus (TmPlus (TmN 10, TmN 4), TmN 2) )
 let t13 = eval (TmMinus (TmPlus (TmN 10, TmN 4), TmN 2) )
-printf "%b" (eval t1 = TmTrue)
-match t10 with
-  | TmN c1 -> printf "%i" c1
+let t18 = eval (TmApp ((TmFun (TmVar "x", (TmPlus (TmVar "x", TmN 2) ) ) ), TmN 5))
+let t19 = TmPlus (TmVar "x", TmN 2)
+
+printf "%b\n" (eval t1 = TmTrue)
+let test = eval (replaceTerm t19 "x" (TmN 3))
+match test with
+  | TmN c1 -> printf "%i\n" c1
   | _ -> printf "nan"
-match t13 with
+match t18 with
   | TmN c1 -> printf "%i" c1
   | _ -> printf "nan"
