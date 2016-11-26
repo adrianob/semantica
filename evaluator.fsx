8b17e927ab84994b38e2199be0a71678377cbcc5
Adriano Benin <adriano.benin@gmail.com>
2016-11-26 01:03:19 -0200

    fix replace

--- evaluator.fsx
+++ evaluator.fsx
@@ -34,10 +34,12 @@ let rec replaceTerm expression ident newTerm =
   | TmVar t1 when (t1 = ident)-> newTerm
   | TmVar _ -> expression
   | TmOp (t1, t2, t3) -> TmOp (t1, replaceTerm t2 ident newTerm, replaceTerm t3 ident newTerm)
+  | TmFun (TmVar t1, t2) when(t1 = ident) -> TmFun (TmVar t1, t2)
+  | TmFun (TmVar t1, t2) when(t1 <> ident) -> TmFun (TmVar t1, replaceTerm t2 ident newTerm)
   | TmIf ( t1 , t2 , t3 ) -> TmIf (replaceTerm t1 ident newTerm, replaceTerm t2 ident newTerm, replaceTerm t3 ident newTerm)
   | TmApp (t1, t2) -> TmApp (replaceTerm t1 ident newTerm, replaceTerm t2 ident newTerm)
-  | TmLet (TmVar ident, t2, t3) -> TmLet (TmVar ident, replaceTerm t2 ident newTerm, t3)
-  | TmLet (t1, t2, t3) -> TmLet (t1, replaceTerm t2 ident newTerm, replaceTerm t3 ident newTerm)
+  | TmLet (TmVar t1, t2, t3) when(t1 = ident) -> TmLet (TmVar ident, replaceTerm t2 ident newTerm, t3)
+  | TmLet (TmVar t1, t2, t3) when(t1 <> ident) -> TmLet (TmVar t1, replaceTerm t2 ident newTerm, replaceTerm t3 ident newTerm)
   | _ -> expression
 
 let isValue t =
@@ -165,11 +167,36 @@ let t12 = eval (TmOp (TmPlus, TmOp (TmPlus, TmN 10, TmN 4), TmN 2) )
 let t13 = eval (TmOp (TmMinus, TmOp (TmPlus, TmN 10, TmN 4), TmN 2) )
 let t18 = eval (TmApp ((TmFun (TmVar "x", (TmOp (TmPlus, TmVar "x", TmN 2) ) ) ), TmN 5))
 let t19 = TmOp (TmPlus, TmVar "x", TmN 2)
+let t22 = eval (TmLet (TmVar "foo", TmFun (TmVar "y", TmOp (TmPlus, TmN 5, TmVar "y")), TmApp (TmVar "foo", TmN 10)))
+(*
+let x = 2 in
+  let foo = (fn y => x+y) in
+    let x = 5 in
+      foo (10)
+*)
+let t20 = eval (TmLet (TmVar "x", TmN 2, TmLet (TmVar "foo", TmFun (TmVar "y", TmOp (TmPlus, TmVar "x", TmVar "y")) , TmLet (TmVar "x", TmN 5, TmApp (TmVar "foo", TmN 10)))))
+let t21 = eval (TmLet (TmVar "x",
+                       TmN 2,
+                       TmLet (TmVar "foo",
+                              TmFun (TmVar "y", TmOp (TmPlus, TmVar "x", TmVar "y")),
+                              TmApp (TmVar "foo", TmN 10)
+                              )
+                       )
+)
 
 let test = eval (replaceTerm t19 "x" (TmN 3))
 match test with
   | TmN c1 -> printf "%i\n" c1
   | _ -> printf "nan"
 match t18 with
-  | TmN c1 -> printf "%i" c1
+  | TmN c1 -> printf "%i\n" c1
+  | _ -> printf "nan"
+match t20 with
+  | TmN c1 -> printf "%i\n" c1
+  | _ -> printf "nan"
+match t22 with
+  | TmN c1 -> printf "%i\n" c1
+  | _ -> printf "nan"
+match t21 with
+  | TmN c1 -> printf "%i\n" c1
   | _ -> printf "nan"
